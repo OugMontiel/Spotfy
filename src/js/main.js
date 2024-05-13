f1e22173324d0677ade3fecab41b2a913f12b3f8
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-05-13 11:56:10 -0500

    Merge branch 'S1'

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import {
+    s1a1,
+    s1a2,
+    s1a3
+} from "./components/s1.js"
+
+
+customElements.define("s1-a1", s1a1 )
+customElements.define("s1-a2", s1a2 )
+customElements.define("s1-a3", s1a3 )
